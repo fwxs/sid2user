59b25c76b016b2e52e810d157d31c29cc97b27ac
Mr Pacman <mrpcmn@protonmail.com>
2018-05-11 00:09:13 -0500

    Update sid2user.py

--- sid2user.py
+++ sid2user.py
@@ -2,26 +2,8 @@ import winreg
 import sys
 
 __author__ = "pacmanator"
-__email__ = "mrpacmanator@gmail.com"
+__email__ = "mrpacmanator at gmail dot com"
 __version__ = "v1.0"
-
-"""
-    Map a Windows user identifier to a username.
-    Copyright (C) 2018 pacmanator
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
 
 
 def get_normal_user_name(user_sid):
